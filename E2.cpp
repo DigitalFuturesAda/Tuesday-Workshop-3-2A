70d8e7a6ca38fbc2ce805d05da34283491c2ea68
Lyons <58537339+DigitalFuturesAda@users.noreply.github.com>
2021-01-12 13:26:54 +0000

    Create E2.cpp

---
+++ E2.cpp
@@ -0,0 +1,22 @@
+https://repl.it/@B3GHU8/3-2A-E1-1#main.cpp
+
+#include <iostream>
+#include <algorithm>
+
+void convertToLowerCase(std::string &str) {
+    // converts string to lowercase by iterating through each char and calling tolower on it
+    std::transform(str.begin(), str.end(), str.begin(),
+                   [](unsigned char c){ return std::tolower(c); });
+}
+
+int main() {
+  std::string input;
+  std::cout << "Enter a word: ";
+
+  getline(std::cin, input);
+
+  std::cout << input << " converted to lowercase is: ";
+  convertToLowerCase(input);
+
+  std::cout << input << std::endl;
+}
